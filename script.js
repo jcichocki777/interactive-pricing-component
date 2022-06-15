3f553a15f25ee4e179d4a2cf3a4d77aefba3490a
jcichocki777 <jcichockigh@gmail.com>
2022-06-15 16:33:27 +0200

    HTML/CSS/JS

---
+++ script.js
@@ -0,0 +1,19 @@
+const mySlider = document.querySelector(".slider");
+const sliderVal = document.querySelector(".slider-value span");
+const toggleBtn = document.querySelector(".toggle");
+
+function slider() {
+  valPercent = (mySlider.value / mySlider.max) * 100;
+  mySlider.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) ${valPercent}%, hsl(224, 65%, 95%) ${valPercent}%)`;
+  //   sliderVal.textContent = `$${mySlider.value}`;
+  calcYearlyBilling();
+}
+
+function calcYearlyBilling() {
+  const yearlyBillingValue = Number((mySlider.value * 0.75 * 12).toFixed(2));
+  if (toggleBtn.checked) {
+    sliderVal.textContent = `$${yearlyBillingValue}`;
+  } else {
+    sliderVal.textContent = `$${mySlider.value}`;
+  }
+}
